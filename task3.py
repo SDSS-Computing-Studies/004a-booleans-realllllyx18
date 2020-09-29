7283ab5c6a13bee0fb546502d351f0e7d0df76d5
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-09-28 17:49:38 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -18,7 +18,7 @@
 # "1" : The number is 10 or a number up to 100 
 # "0" : The number is 0 or a number up to 100 
 
-number=input("nter a number")
+number=input("enter a number")
 
 number=float(number)
 
